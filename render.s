861eb67b524e2649d9d7d348b30c296c9dec7554
Michael Mackus <michaelmackus@gmail.com>
2018-10-11 18:05:50 -0700

    Turn off rendering before rendering map
    This is the "recommended" way to do full screen nametable updates since
    the NES doesn't have enough time during VBLANK to update the entire
    screen. Will need to utilize draw buffer for further updates to NT.

--- render.s
+++ render.s
@@ -7,6 +7,9 @@
 .proc render
 
 generate_ppu:
+    ; turn off rendering
+    lda #%00000000 ; note: need second bit in order to show background on left side of screen
+    sta $2001
     ; prep ppu for first nametable write
     lda #$20
     sta $2006
@@ -21,25 +24,45 @@ bg_bits:
     beq next_bg
     iny
     asl
-    bcs wall
-    ; push accumulator to stack, draw floor, then pull from stack
+    bcs floor
+    ; push accumulator to stack, draw bg, then pull from stack
     pha
     lda #$00
     sta $2007
     pla
     jmp bg_bits
-wall:
-    ; push accumulator to stack, draw wall, then pull from stack
+floor:
+    ; push accumulator to stack, draw floor, then pull from stack
     pha
-    lda #$60
+    lda #$82
     sta $2007
     pla
     jmp bg_bits
 next_bg:
     inx
     ; repeat until desired amount (first byte of sprite-set)
-    cpx #$78
+    ; todo sometimes rendering weird tile to bottom right...
+    ; todo perhaps try turning of sprites to see where its coming from?
+    cpx #96
     bne bg_repeat
+    ; fill the rest of the tiles to BG for now
+    ldy #0
+fill_bg:
+    lda #$00
+    sta $2007
+    inx
+    cpx #00
+    bne fill_bg
+    ; reset x & increment y
+    ldx #0
+    iny
+    cpy #3
+    beq last_page
+    bcc bgdone ; greater than 3, we're done
+    jmp fill_bg
+last_page:
+    ldx #64
+    jmp fill_bg
 bgdone:
     ; tell PPU to render BG & sprites
     lda #%00011010 ; note: need second bit in order to show background on left side of screen
